6e18b55cca40f8f4b8f3c840a01a26721a857722
Philipinho <16838612+Philipinho@users.noreply.github.com>
2023-03-31 02:03:20 +0100

    Update functions

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -2,18 +2,97 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\DocumentResource;
+use App\Traits\DocumentTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 
 class DocumentController extends Controller
 {
-    public function show(): \Inertia\Response
+    use DocumentTrait;
+
+    public function show(Request $request): \Inertia\Response
     {
-        return Inertia::render('Documents/Show');
+
+        $templates = config('templates');
+        $categories = config('categories');
+
+        $values = [
+            'name' => $request->input('name'),
+            'language' => $request->input('language'),
+            'variants' => $request->input('variants', 1),
+            'creativity' => $request->input('creativity', 'original'),
+            'type' => $request->input('type', 'summarize'),
+        ];
+
+        foreach ($templates as $key => $value) {
+            if ($value['single_input']) {
+                $values[$key . '_input'] = $request->input($key . '_input');
+            } else {
+                foreach ($value['inputs'] as $k => $v) {
+                    $values[$k] = $request->input($k);
+                }
+            }
+        }
+
+        $data = (object)[
+            'values' => $values,
+            'templates' => $templates,
+            'categories' => $categories,
+        ];
+
+        return Inertia::render('Documents/Show', compact('data'));
     }
 
-    public function create()
+    public function create(Request $request)
     {
+
+        $request->validate([
+            'name' => 'required|string',
+            'type' => 'required|string',
+        ]);
+
+        $templates = config('templates');
+        $categories = config('categories');
+
+        $language = mb_strtolower(config('languages')[$request->input('language')]['iso']);
+
+        // TODO: validate max words
+
+        $input = null;
+        $prompt = "Write an answer in {$language}. ";
+
+        if ($templates[$request->input('type')]['single_input']) {
+            $input = $request->input($request->input('type') . '_input');
+            $prompt .= sprintf($templates[$request->input('type')]['prompt'], $input);
+        } else {
+            $inputs = [];
+            foreach ($templates[$request->input('type')]['inputs'] as $key => $value) {
+                $inputs[$key] = $request->input($key);
+            }
+
+            $prompt .= vsprintf($templates[$request->input('type')]['prompt'], array_values($inputs));
+            $input = json_encode($inputs);
+        }
+
+        try {
+            $created = $this->documentsStore($request, $prompt);
+            if ($created) {
+                return DocumentResource::make($created);
+            }
+        } catch (\Exception $e) {
+            Log::error("Documents store error:  {$e}");
+            return response()->json([
+                'message' => __('An unexpected error has occurred, please try again.'),
+                'status' => 500
+            ], 500);
+        }
+
+        return response()->json([
+            'message' => __('Resource not found.'),
+            'status' => 404
+        ], 404);
 
     }
 
