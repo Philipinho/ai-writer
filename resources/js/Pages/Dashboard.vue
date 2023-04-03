9d0547cb207750ef2bb47d605542701e6710f38d
Philipinho <16838612+Philipinho@users.noreply.github.com>
2023-04-03 17:11:39 +0100

    Separate components

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,13 +8,15 @@ import ButtonIcon from '@/Components/ButtonIcon.vue';
 
 <template>
     <AppLayout title="Dashboard">
+        <!--
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                 Dashboard
             </h2>
         </template>
+        -->
 
-        <div class="py-12">
+        <div class="py-6">
 
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
--- resources/js/Pages/Templates/Index.vue
+++ resources/js/Pages/Templates/Index.vue
@@ -1,33 +1,15 @@
 <script>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import {EnvelopeIcon, PhoneIcon} from '@heroicons/vue/20/solid'
-import TextInput from '@/Components/TextInput.vue';
-
+import TemplateList from '@/Pages/Templates/Partials/TemplateList.vue'
 
 export default {
     components: {
         AppLayout,
-        TextInput,
+        TemplateList,
     },
     props: {
         templates: Object,
     },
-    data() {
-        return {
-            search: '',
-        }
-    },
-    computed: {
-        filteredTemplates() {
-            return this.templates.filter(
-                template => {
-                    return template.name.toLowerCase().includes(this.search.toLowerCase())
-                        || template.description.toLowerCase().includes(this.search.toLowerCase());
-                }
-            )
-        }
-    }
-
 }
 
 </script>
@@ -42,47 +24,7 @@ export default {
 
         <div>
 
-            <div class="pb-6 max-w-sm">
-                <div class="relative flex items-center">
-                    <div
-                        class="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400 pl-3 flex items-center justify-center"
-                        style="font-size: 24px" aria-hidden="true">
-                        <i class="ri-search-line"></i>
-                    </div>
-
-                    <TextInput
-                        v-model="search"
-                        name="search"
-                        type="search"
-                        class="mt-1 block w-full pl-8"
-                        placeholder="Search.."
-                    />
-                </div>
-            </div>
-
-            <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-                <li v-for="template in filteredTemplates"
-                    class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
-
-                    <a :href="route('document.new', {'template': template.key})"
-                       class="flex w-full items-center justify-between space-x-6 p-6">
-                        <div class="flex-1 truncate">
-                            <div class="flex items-center space-x-3">
-                                <h3 class="truncate text-sm font-medium text-gray-900">{{ template.name }}</h3>
-                                <span
-                                    class="inline-block flex-shrink-0 rounded-full bg-green-100 px-2 py-0.5 text-xs font-medium text-green-800">
-                                        {{ template.key }}</span>
-                            </div>
-                            <p class="mt-1 truncate text-sm text-gray-500">{{ template.description }}</p>
-                        </div>
-
-                        <div class="h-10 w-10 rounded-full bg-green-100 flex items-center justify-center">
-                            <div style="font-size: 24px;"><i :class="template.icon"></i></div>
-                        </div>
-
-                    </a>
-                </li>
-            </ul>
+            <TemplateList :templates="templates" />
 
         </div>
     </AppLayout>
