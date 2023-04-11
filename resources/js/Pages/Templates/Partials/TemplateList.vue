fe205f4dc1885fd16121339fca4d412387903c2b
Philipinho <16838612+Philipinho@users.noreply.github.com>
2023-04-11 14:22:24 +0100

    Remove placeholders

--- resources/js/Pages/Templates/Partials/TemplateList.vue
+++ resources/js/Pages/Templates/Partials/TemplateList.vue
@@ -1,18 +1,9 @@
 <script>
 import TextInput from '@/Components/TextInput.vue';
-import {
-    AcademicCapIcon,
-    BanknotesIcon,
-    CheckBadgeIcon,
-    ClockIcon,
-    ReceiptRefundIcon,
-    UsersIcon
-} from "@heroicons/vue/24/outline";
 
 export default {
     components: {
         TextInput,
-        CheckBadgeIcon
     },
     props: {
         templates: Object,
@@ -20,50 +11,6 @@ export default {
     data() {
         return {
             search: '',
-            actions: [
-                {
-                    title: 'Request time off',
-                    href: '#',
-                    icon: ClockIcon,
-                    iconForeground: 'text-teal-700',
-                    iconBackground: 'bg-teal-50',
-                },
-                {
-                    title: 'Benefits',
-                    href: '#',
-                    icon: CheckBadgeIcon,
-                    iconForeground: 'text-purple-700',
-                    iconBackground: 'bg-purple-50',
-                },
-                {
-                    title: 'Schedule a one-on-one',
-                    href: '#',
-                    icon: UsersIcon,
-                    iconForeground: 'text-sky-700',
-                    iconBackground: 'bg-sky-50',
-                },
-                {
-                    title: 'Payroll',
-                    href: '#',
-                    icon: BanknotesIcon,
-                    iconForeground: 'text-yellow-700',
-                    iconBackground: 'bg-yellow-50',
-                },
-                {
-                    title: 'Submit an expense',
-                    href: '#',
-                    icon: ReceiptRefundIcon,
-                    iconForeground: 'text-rose-700',
-                    iconBackground: 'bg-rose-50',
-                },
-                {
-                    title: 'Training',
-                    href: '#',
-                    icon: AcademicCapIcon,
-                    iconForeground: 'text-indigo-700',
-                    iconBackground: 'bg-indigo-50',
-                },
-            ]
         }
     },
     computed: {
