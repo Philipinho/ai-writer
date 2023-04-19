372d49a05f4b5821863876e4a0c92678973b77a0
Philipinho <16838612+Philipinho@users.noreply.github.com>
2023-04-20 00:06:11 +0100

    new migrations - replacing Stripe Cashier

--- database/migrations/2023_04_08_000002_create_subscriptions_table.php
+++ database/migrations/2023_04_08_000002_create_subscriptions_table.php
@@ -14,13 +14,23 @@ return new class extends Migration
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('team_id');
-            $table->string('name');
+            $table->foreignId('plan_id')->nullable();
+            $table->string('stripe_customer_id')->unique();
             $table->string('stripe_id')->unique();
             $table->string('stripe_status');
-            $table->string('stripe_price')->nullable();
+            $table->string('stripe_price_id')->nullable();
+            $table->string('stripe_product_id')->nullable();
+            $table->string('stripe_item_id')->nullable();
+            $table->integer('amount')->nullable();
+            $table->string('currency')->nullable();
             $table->integer('quantity')->nullable();
+            $table->string('interval')->nullable();
             $table->timestamp('trial_ends_at')->nullable();
+            $table->datetime('start_date')->nullable();
             $table->timestamp('ends_at')->nullable();
+            $table->datetime('next_payment_date')->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->softDeletes();
             $table->timestamps();
 
             $table->index(['team_id', 'stripe_status']);
---
+++ database/migrations/2023_04_19_211724_create_plans_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->text('features')->nullable();
+            $table->text('payment_gateway')->nullable();
+            $table->string('product_id')->nullable();
+            $table->string('monthly_id')->nullable();
+            $table->string('yearly_id')->nullable();
+            $table->string('monthly_price')->nullable();
+            $table->string('yearly_price')->nullable();
+            $table->string('currency')->nullable();
+            $table->integer('word_limit')->default(0);
+            $table->integer('credits')->default(0);
+            $table->integer('seats')->default(0);
+            $table->integer('trial_days')->nullable();
+            $table->boolean('featured')->default(false);
+            $table->boolean('free')->default(false);
+            $table->smallInteger('order')->default(0);
+            $table->tinyInteger('status')->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plans');
+    }
+};
