ca391bd09474e432fb29df6e3f86226adc6412c0
Philipinho <16838612+Philipinho@users.noreply.github.com>
2023-04-23 14:41:46 +0100

    Contact page

--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -1,3 +1,16 @@
 <x-appLayout :title="$meta->title">
 
+    <div class="max-w-7xl px-8 mx-auto lg:px-20 py-24">
+        <h1 class="py-5">Contact us</h1>
+
+
+        <div class="relative flex flex-col min-w-0 rounded break-words shadow-soft p-6 md:p-12">
+
+            If you have any issues, questions or comments, please contact us by sending a mail to support@copyphrase.com
+
+        </div>
+
+    </div>
+
+
 </x-appLayout>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-white">
-    <div class="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+    <div class="mx-auto max-w-7xl overflow-hidden px-6 py-6 lg:px-8">
         <nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
             <!--
             <div class="pb-6">
@@ -65,6 +65,6 @@
             </a>
             -->
         </div>
-        <p class="mt-10 text-center text-xs leading-5 text-gray-500">&copy; {{ date('Y') }} CopyPhrase. All rights reserved.</p>
+        <p class="mt-5 text-center text-xs leading-5 text-gray-500">&copy; {{ date('Y') }} CopyPhrase. All rights reserved.</p>
     </div>
 </footer>
